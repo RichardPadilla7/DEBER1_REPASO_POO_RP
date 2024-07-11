7e54bf9288c8e5e3db5091950333dfe94353c36f
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-07-10 20:38:24 -0500

    finalizado el codigo

--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Estudiantes {
+//Richard Padilla
+public class Estudiantes {
+    String nombre;
+    String cedula;
+    int bimestreUno;
+    int bimestreDos;
+
+    public Estudiantes(String nombre, String cedula, int bimestreUno, int bimestreDos) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.bimestreUno = bimestreUno;
+        this.bimestreDos = bimestreDos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public int getBimestreUno() {
+        return bimestreUno;
+    }
+
+    public int getBimestreDos() {
+        return bimestreDos;
+    }
+
+    public int getPromedio() {
+        return (bimestreUno + bimestreDos) / 2;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Richard Padilla
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:mysql://localhost:3306/estudiantes2024a";
+        String user = "root";
+        String password = "123456";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Estudiantes> estudiantes = new ArrayList<>();
+
+        String query = "SELECT * FROM estudiantes";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Conectado a la base de datos");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String nombre = resultSet.getString("nombre");
+                String cedula = resultSet.getString("cedula");
+                int bimestreUno = resultSet.getInt("b1");
+                int bimestreDos = resultSet.getInt("b2");
+
+                Estudiantes estudiante = new Estudiantes(nombre, cedula, bimestreUno, bimestreDos);
+                estudiantes.add(estudiante);
+            }
+
+            for (Estudiantes estudiante : estudiantes) {
+                System.out.println("Nombre: " + estudiante.getNombre() +
+                        ", Cedula: " + estudiante.getCedula() +
+                        ", Bimestre 1: " + estudiante.getBimestreUno() +
+                        ", Bimestre 2: " + estudiante.getBimestreDos() +
+                        ", Promedio: " + estudiante.getPromedio());
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error de los datos!." + ex.getMessage());
         }
     }
 }
